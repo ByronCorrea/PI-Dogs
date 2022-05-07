3c26263d3e7f4e2907718a9affb290ca6394e286
ByronCorrea <byron10-@hotmail.com>
2022-05-06 22:44:48 -0300

    Created API

---
+++ api/src/utils/getDataApi.js
@@ -0,0 +1,33 @@
+// const { Dog } = require("../db");
+const axios = require("axios");
+const { API_KEY } = process.env;
+
+async function getDataApi() {
+  // Trae toda la data
+  let response = await axios.get(
+    `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
+  );
+  response = response.data.map((el) => {
+    const pesoTemp = el.weight.metric.split("-");
+    const alturaTemp = el.height.metric.split("-");
+    const imagen = el.image.url;
+    return {
+      id: el.id,
+      name: el.name,
+      height_min: alturaTemp[0],
+      height_max: alturaTemp[1],
+      weight_min: pesoTemp[0],
+      weight_max: pesoTemp[1],
+      life_span: el.life_span,
+      image: imagen,
+      userCreate: false,
+      temperaments: el.temperament,
+    };
+  });
+
+  return response;
+}
+
+module.exports = {
+  getDataApi,
+};
