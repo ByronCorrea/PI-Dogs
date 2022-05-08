bd231ab0cde92e2e01d9030b5be7f09a44d4c328
ByronCorrea <82012482+ByronCorrea@users.noreply.github.com>
2022-05-07 23:01:08 -0300

    Revert "Api"

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,15 +1,14 @@
 const { Router } = require("express");
-const dogsRoute = require("./dogs");
-const tempermentsRoutes = require("./temperaments");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const dogs = require("./dogs.js");
+const temperament = require("./temperament.js");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-router.use("/dogs", dogsRoute);
-router.use("/temperaments", tempermentsRoutes);
+router.use("/dogs", dogs);
+router.use("/temperament", temperament);
 
 module.exports = router;
--- client/src/containers/App.js
+++
@@ -1,7 +0,0 @@
-import styles from "./App.module.css";
-
-function App() {
-  return <div className={styles.App}>Hello world!</div>;
-}
-
-export default App;
